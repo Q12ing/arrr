46dead9b13540adcba2f1342175fa9ec576f435c
Q12ing <simjang640@gmail.com>
2025-03-14 15:49:49 +0900

    Add files via upload

--- scripts/common.js
+++ scripts/common.js
@@ -9,3 +9,13 @@ dongwonGroup.addEventListener('click',function(e){
 groupList.addEventListener('mouseleave',function(){
     groupList.classList.remove('active');
 })
+
+/* 전체 a태그 새로고침 안되게 하기 */
+const aTag=document.querySelectorAll('body a')
+console.log(aTag[1]);
+
+aTag.forEach(item => {
+    item.addEventListener('click',function(e){
+        e.preventDefault();
+    })
+});
